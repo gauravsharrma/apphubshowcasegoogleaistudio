737978c4848ddce0a99a313b15a4927ab8ddfe08
gauravsharrma <gauravsharrma00@gmail.com>
2025-06-25 11:45:14 +0400

    Add files via upload

---
+++ components/icons/InformationIcon.tsx
@@ -0,0 +1,8 @@
+
+import React from 'react';
+
+export const InformationIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
+  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" {...props}>
+    <path strokeLinecap="round" strokeLinejoin="round" d="M12 18.75a.75.75 0 100-1.5.75.75 0 000 1.5zM11.25 6.75A.75.75 0 0012 6h.008c.414 0 .75.336.75.75v5.25a.75.75 0 001.5 0V6.75a2.25 2.25 0 00-2.25-2.25H12a2.25 2.25 0 00-2.25 2.25v.519c0 .463.207.9.554 1.209l1.5 1.5Zm.75-3.75h.008v.008H12V3zM12 21a9 9 0 100-18 9 9 0 000 18z" />
+  </svg>
+);
